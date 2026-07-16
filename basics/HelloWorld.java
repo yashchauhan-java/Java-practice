e764543d5ce1e398c171bb0445b05562d555feed
Yash Chauhan <yashchauhan7988@gmail.com>
2026-07-16 16:54:44 +0530

    clean setup - java practice journey

--- basics/HelloWorld.java
+++ basics/HelloWorld.java
@@ -1,5 +1,5 @@
-public class HelloWorld{
-    public static void main(String[]args){
+public class HelloWorld {
+    public static void main(String[] args) {
         System.out.println("hello! I'm Yash");
         System.out.println("Starting my Java Full Stack");
     }

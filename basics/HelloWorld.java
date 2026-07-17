1779f0bb3942134ed0c730ed36714d55430c64c1
Yash Chauhan <yashchauhan7988@gmail.com>
2026-07-17 10:26:49 +0530

    web development journey - day 1

--- basics/HelloWorld.java
+++
@@ -1,6 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("hello! I'm Yash");
-        System.out.println("Starting my Java Full Stack");
-    }
-}
